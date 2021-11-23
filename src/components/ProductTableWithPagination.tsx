d016d12c0ab60d14a88b6717f30c3483eb22a252
nicofzzn <nicofaza975@gmail.com>
2021-11-24 02:20:23 +0700

    add table with row selection and pagination

---
+++ src/components/ProductTableWithPagination.tsx
@@ -0,0 +1,224 @@
+import { useMemo } from 'react'
+import { useTable, useRowSelect, CellProps, Column, usePagination } from 'react-table'
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Checkbox,
+  ButtonGroup,
+  Button,
+  Input,
+  Select,
+} from '@chakra-ui/react'
+
+type Prop<T> = {
+  unMemoizedData: T[]
+}
+
+function getColumns<T>(data: T[]) {
+  if (data.length > 0) {
+    const keys = Object.keys(data[0])
+    return keys.map(key => ({
+      Header: key.replace(/([A-Z])/g, ' $1'),
+      accessor: key,
+    }))
+  }
+
+  return []
+}
+
+const ProductTableWithPagination = <T extends {}>({ unMemoizedData }: Prop<T>) => {
+  const data = useMemo(() => unMemoizedData, [unMemoizedData])
+
+  const columns: readonly Column<object>[] = useMemo(
+    () => getColumns(unMemoizedData),
+    [unMemoizedData]
+  )
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    prepareRow,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    selectedFlatRows,
+    state: { pageIndex, pageSize, selectedRowIds },
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { hiddenColumns: ['id'], pageSize: 3 },
+    },
+    usePagination,
+    useRowSelect,
+    hooks => {
+      hooks.visibleColumns.push(columns => [
+        {
+          id: 'selection',
+          Header: ({ getToggleAllPageRowsSelectedProps }) => {
+            const { checked, indeterminate, onChange } =
+              getToggleAllPageRowsSelectedProps()
+            return (
+              <div>
+                <Checkbox
+                  isChecked={checked}
+                  isIndeterminate={indeterminate}
+                  onChange={onChange}
+                />
+              </div>
+            )
+          },
+          Cell: ({ row }: React.PropsWithChildren<CellProps<object, any>>) => {
+            const { checked, onChange } = row.getToggleRowSelectedProps()
+            return (
+              <div>
+                <Checkbox isChecked={checked} onChange={onChange} />
+              </div>
+            )
+          },
+        },
+        ...columns,
+      ])
+    }
+  )
+
+  if (unMemoizedData.length < 1) return <div>no data</div>
+
+  return (
+    <div>
+      <pre>
+        <code>
+          {JSON.stringify(
+            {
+              pageIndex,
+              pageSize,
+              pageCount,
+              canNextPage,
+              canPreviousPage,
+            },
+            null,
+            2
+          )}
+        </code>
+      </pre>
+      <Table {...getTableProps()}>
+        <Thead>
+          {
+            // Loop over the header rows
+            headerGroups.map(headerGroup => (
+              // Apply the header row props
+              <Tr {...headerGroup.getHeaderGroupProps()}>
+                {
+                  // Loop over the headers in each row
+                  headerGroup.headers.map(column => (
+                    // Apply the header cell props
+                    <Th px='3' {...column.getHeaderProps()}>
+                      {
+                        // Render the header
+                        column.render('Header')
+                      }
+                    </Th>
+                  ))
+                }
+              </Tr>
+            ))
+          }
+        </Thead>
+        {/* Apply the table body props */}
+        <Tbody {...getTableBodyProps()}>
+          {
+            // Loop over the table rows
+            page.map(row => {
+              // Prepare the row for display
+              prepareRow(row)
+              return (
+                // Apply the row props
+                <Tr {...row.getRowProps()}>
+                  {
+                    // Loop over the rows cells
+                    row.cells.map(cell => {
+                      // Apply the cell props
+                      return (
+                        <Td p='3' {...cell.getCellProps()}>
+                          {
+                            // Render the cell contents
+                            cell.render('Cell')
+                          }
+                        </Td>
+                      )
+                    })
+                  }
+                </Tr>
+              )
+            })
+          }
+        </Tbody>
+      </Table>
+      <ButtonGroup>
+        <Button disabled={!canPreviousPage} onClick={() => gotoPage(0)}>
+          {'<<'}
+        </Button>
+        <Button disabled={!canPreviousPage} onClick={() => previousPage()}>
+          {'<'}
+        </Button>
+        <Button disabled={!canNextPage} onClick={() => nextPage()}>
+          {'>'}
+        </Button>
+        <Button disabled={!canNextPage} onClick={() => gotoPage(pageCount - 1)}>
+          {'>>'}
+        </Button>
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <Input
+          type='number'
+          defaultValue={pageIndex + 1}
+          onChange={e => {
+            const page = e.target.value ? +e.target.value - 1 : 0
+            console.log(page)
+            gotoPage(page)
+          }}
+        />
+      </ButtonGroup>
+      <div>
+        <span>Page size</span>
+        <Select w='sm' value={pageSize} onChange={e => setPageSize(+e.target.value)}>
+          {[3, 5, 7, 10].map(k => (
+            <option key={k} value={k}>
+              {k}
+            </option>
+          ))}
+        </Select>
+      </div>
+      <pre>
+        <code>
+          {JSON.stringify(
+            {
+              selectedRowIds: selectedRowIds,
+              'selectedFlatRows[].original': selectedFlatRows.map(d => d.original),
+            },
+            null,
+            2
+          )}
+        </code>
+      </pre>
+      {/* {console.log(selectedFlatRows.map(row => row.original))} */}
+    </div>
+  )
+}
+
+export default ProductTableWithPagination
--- src/components/filterComponents/FilterResult.tsx
+++ src/components/filterComponents/FilterResult.tsx
@@ -1,5 +1,169 @@
 import { HStack, Spacer, Box } from '@chakra-ui/react'
 import ProductTable from '../ProductTable'
+import ProductTableWithPagination from '../ProductTableWithPagination'
+
+const data = [
+  {
+    id: '2oiiosdvso',
+    name: 'Asrock a320m HDV',
+    formFactor: 'm-Atx',
+    memorySlots: 2,
+    socket: 'AM4',
+    price: 750000,
+  },
+  {
+    id: 'ffhasikudfs',
+    name: 'MSI B450-M',
+    formFactor: 'Atx',
+    memorySlots: 4,
+    socket: 'AM4',
+    price: 1500000,
+  },
+  {
+    id: '2oiiosdvso',
+    name: 'Asrock a320m HDV',
+    formFactor: 'm-Atx',
+    memorySlots: 2,
+    socket: 'AM4',
+    price: 750000,
+  },
+  {
+    id: 'ffhasikudfs',
+    name: 'MSI B450-M',
+    formFactor: 'Atx',
+    memorySlots: 4,
+    socket: 'AM4',
+    price: 1500000,
+  },
+  {
+    id: '2oiiosdvso',
+    name: 'Asrock a320m HDV',
+    formFactor: 'm-Atx',
+    memorySlots: 2,
+    socket: 'AM4',
+    price: 750000,
+  },
+  {
+    id: 'ffhasikudfs',
+    name: 'MSI B450-M',
+    formFactor: 'Atx',
+    memorySlots: 4,
+    socket: 'AM4',
+    price: 1500000,
+  },
+  {
+    id: '2oiiosdvso',
+    name: 'Asrock a320m HDV',
+    formFactor: 'm-Atx',
+    memorySlots: 2,
+    socket: 'AM4',
+    price: 750000,
+  },
+  {
+    id: 'ffhasikudfs',
+    name: 'MSI B450-M',
+    formFactor: 'Atx',
+    memorySlots: 4,
+    socket: 'AM4',
+    price: 1500000,
+  },
+  {
+    id: '2oiiosdvso',
+    name: 'Asrock a320m HDV',
+    formFactor: 'm-Atx',
+    memorySlots: 2,
+    socket: 'AM4',
+    price: 750000,
+  },
+  {
+    id: 'ffhasikudfs',
+    name: 'MSI B450-M',
+    formFactor: 'Atx',
+    memorySlots: 4,
+    socket: 'AM4',
+    price: 1500000,
+  },
+  {
+    id: '2oiiosdvso',
+    name: 'Asrock a320m HDV',
+    formFactor: 'm-Atx',
+    memorySlots: 2,
+    socket: 'AM4',
+    price: 750000,
+  },
+  {
+    id: 'ffhasikudfs',
+    name: 'MSI B450-M',
+    formFactor: 'Atx',
+    memorySlots: 4,
+    socket: 'AM4',
+    price: 1500000,
+  },
+  {
+    id: '2oiiosdvso',
+    name: 'Asrock a320m HDV',
+    formFactor: 'm-Atx',
+    memorySlots: 2,
+    socket: 'AM4',
+    price: 750000,
+  },
+  {
+    id: 'ffhasikudfs',
+    name: 'MSI B450-M',
+    formFactor: 'Atx',
+    memorySlots: 4,
+    socket: 'AM4',
+    price: 1500000,
+  },
+  {
+    id: '2oiiosdvso',
+    name: 'Asrock a320m HDV',
+    formFactor: 'm-Atx',
+    memorySlots: 2,
+    socket: 'AM4',
+    price: 750000,
+  },
+  {
+    id: 'ffhasikudfs',
+    name: 'MSI B450-M',
+    formFactor: 'Atx',
+    memorySlots: 4,
+    socket: 'AM4',
+    price: 1500000,
+  },
+  {
+    id: '2oiiosdvso',
+    name: 'Asrock a320m HDV',
+    formFactor: 'm-Atx',
+    memorySlots: 2,
+    socket: 'AM4',
+    price: 750000,
+  },
+  {
+    id: 'ffhasikudfs',
+    name: 'MSI B450-M',
+    formFactor: 'Atx',
+    memorySlots: 4,
+    socket: 'AM4',
+    price: 1500000,
+  },
+  {
+    id: '2oiiosdvso',
+    name: 'Asrock a320m HDV',
+    formFactor: 'm-Atx',
+    memorySlots: 2,
+    socket: 'AM4',
+    price: 750000,
+  },
+  {
+    id: 'ffhasikudfs',
+    name: 'MSI B450-M',
+    formFactor: 'Atx',
+    memorySlots: 4,
+    socket: 'AM4',
+    price: 1500000,
+  },
+]
 
 const FilterResult = () => {
   return (
@@ -8,26 +172,8 @@ const FilterResult = () => {
         <Spacer />
       </HStack>
       <Box>
-        <ProductTable
-          unMemoizedData={[
-            {
-              id: '2oiiosdvso',
-              name: 'Asrock a320m HDV',
-              formFactor: 'm-Atx',
-              memorySlots: 2,
-              socket: 'AM4',
-              price: 750000,
-            },
-            {
-              id: 'ffhasikudfs',
-              name: 'MSI B450-M',
-              formFactor: 'Atx',
-              memorySlots: 4,
-              socket: 'AM4',
-              price: 1500000,
-            },
-          ]}
-        />
+        <ProductTableWithPagination unMemoizedData={data} />
+        {/* <ProductTable unMemoizedData={data} /> */}
       </Box>
     </>
   )
